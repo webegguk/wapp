029d0eb393a722d969f584731375b724dd7ab53b
webegguk <info@webegg.co.uk>
2017-12-03 21:56:24 +0000

    Syncing with firebase database and deploying to wapp-71d68.firebaseapp.com

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
 // import { Route, Link } from 'react-router-dom'
 // import Home from './scripts/components/Home'
-import Routes from './Routes'
+import Routes from './Routes';
+import fire from './fire';
 
 class App extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = { messages: [] }; // <- set up react state
+    }
+    componentWillMount(){
+        /* Create reference to messages in Firebase Database */
+        let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
+        messagesRef.on('child_added', snapshot => {
+            /* Update React state when message is added at Firebase Database */
+            let message = { text: snapshot.val(), id: snapshot.key };
+            this.setState({ messages: [message].concat(this.state.messages) });
+        })
+    }
+    addMessage(e){
+        e.preventDefault(); // <- prevent form submit from reloading the page
+        /* Send the message to Firebase */
+        fire.database().ref('messages').push( this.inputEl.value );
+        this.inputEl.value = ''; // <- clear the input
+    }
     render() {
         return (
             <div>
                 <Routes/>
+                <form onSubmit={this.addMessage.bind(this)}>
+                    <input type="text" ref={ el => this.inputEl = el }/>
+                    <input type="submit"/>
+                    <ul>
+                        { /* Render the list of messages */
+                            this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
+                        }
+                    </ul>
+                </form>
             </div>
         )
     }
